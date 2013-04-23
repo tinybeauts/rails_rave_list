1a2581aa8b4aebf1c321bb66a930af7d8bf91b53
dbc gogo <dbc@dbc.com>
2013-04-22 17:07:58 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRaveList::Application.config.secret_token = '91247534d99fe197889b7bf199c2c43fc33474de2d82e7c7098b0304b605d3ab5daf84912e25dc951b6b42e7bad35bf9f0fdd173673e5e8be4485b4869315073'
